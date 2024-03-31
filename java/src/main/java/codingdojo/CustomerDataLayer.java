8e298beda06a981b560c50c67dc7b4e04dc29ca4
Boris Kukec <boriskukec71@gmail.com>
2024-03-31 16:04:03 +0200

    Return of DataAccess class 2

--- java/src/main/java/codingdojo/CustomerDataLayer.java
+++ java/src/main/java/codingdojo/CustomerDataLayer.java
@@ -2,14 +2,6 @@ package codingdojo;
 
 public interface CustomerDataLayer {
 
-    default Customer saveCustomerRecord(Customer customer) {
-        if (customer.getInternalId() == null ) {
-            return createCustomerRecord(customer);
-        } else {
-            return updateCustomerRecord(customer);
-        }
-    }
-
     Customer updateCustomerRecord(Customer customer);
 
     Customer createCustomerRecord(Customer customer);
